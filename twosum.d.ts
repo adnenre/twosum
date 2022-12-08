8d1f94332d097fb26932ac1d6ea5d96e513aaf16
adnen rebai <adnen.rebai@gmail.com>
2022-12-08 07:00:42 +0100

    generate declaration types and update version

---
+++ twosum.d.ts
@@ -0,0 +1,1 @@
+declare module '@adnen/twosum'
---
+++ types/main.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Given an array of integers nums and an integer target,
+ * return indices of the two numbers such that they add up to target.
+ */
+declare const twoSum: (nums: number[], target: number) => number[][];
+export default twoSum;
