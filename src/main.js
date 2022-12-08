0c93749f66003ba007915b5dbf727eaddfaae267
adnen rebai <adnen.rebai@gmail.com>
2022-12-08 09:39:01 +0100

    use inline build to NodeNext

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
-"use strict";
 /**
  * Given an array of integers nums and an integer target,
  * return indices of the two numbers such that they add up to target.
  */
-exports.__esModule = true;
-var twoSum = function (nums, target) {
-    var indexs = [];
-    var memo = {};
-    for (var i = 0; i < nums.length; i++) {
-        var current = nums[i];
-        var complement = target - nums[i];
+const twoSum = (nums, target) => {
+    const indexs = [];
+    const memo = {};
+    for (let i = 0; i < nums.length; i++) {
+        let current = nums[i];
+        let complement = target - nums[i];
         if (complement in memo) {
             indexs.push([memo[complement], i]);
         }
@@ -17,4 +15,4 @@ var twoSum = function (nums, target) {
     }
     return indexs;
 };
-exports["default"] = twoSum;
+export default twoSum;
